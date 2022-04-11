bf11942793889182896b9c2f0038e1db020dedb3
Sara Borges <sarakborges@outlook.com>
2022-04-11 11:14:55 -0300

    added carousel

--- src/Components/Templates/Home/index.tsx
+++ src/Components/Templates/Home/index.tsx
@@ -1,4 +1,11 @@
 import React, { FC } from 'react'
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from 'pure-react-carousel'
 
 import { Container } from 'Components/Atoms'
 import { Header, Footer } from 'Components/Organisms'
@@ -11,7 +18,19 @@ export const HomeTemplate: FC = () => {
       <Header />
 
       <Container>
-        <Styled.HomeContent>asd</Styled.HomeContent>
+        <Styled.HomeContent>
+          <CarouselProvider
+            naturalSlideWidth={100}
+            naturalSlideHeight={25}
+            totalSlides={3}
+          >
+            <Slider>
+              <Slide index={0}>I am the first Slide.</Slide>
+              <Slide index={1}>I am the second Slide.</Slide>
+              <Slide index={2}>I am the third Slide.</Slide>
+            </Slider>
+          </CarouselProvider>
+        </Styled.HomeContent>
       </Container>
 
       <Footer />
--- src/Components/Templates/Home/style.ts
+++ src/Components/Templates/Home/style.ts
@@ -3,4 +3,12 @@ import styled from 'styled-components'
 export const HomeContent = styled.div`
   min-height: calc(100vh - 46px - 95px);
   padding: 32px 0;
+
+  .carousel {
+    overflow: hidden;
+
+    .carousel__slider-tray {
+      display: flex;
+    }
+  }
 `
